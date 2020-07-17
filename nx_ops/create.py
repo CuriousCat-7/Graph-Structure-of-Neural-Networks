4cf21a6fc1beef4940e3a43c2442ec8795fc2f05
limingyao <limingyao@ainirobot.com>
2020-07-17 15:28:32 +0800

    compression

--- nx_ops/create.py
+++ nx_ops/create.py
@@ -8,17 +8,17 @@ def _dump(f, path, overwrite):
         logger.warning("{} already exists", path)
         return
     logger.info(path)
-    f().to_csv(path)
+    f().to_csv(path, compression="gz")
 
 
 def create_all(save_root="save/csv/", overwrite=False):
     os.makedirs(save_root, exist_ok=True)
-    _dump(sample_harary, os.path.join(save_root, "harary-paper.csv"), overwrite)
-    _dump(sample_ring, os.path.join(save_root, "ring-paper.csv"), overwrite)
-    _dump(sample_er, os.path.join(save_root, "er-paper.csv"), overwrite)
-    _dump(sample_ba, os.path.join(save_root, "ba-paper.csv"), overwrite)
-    _dump(sample_ws, os.path.join(save_root, "ws-paper.csv"), overwrite)
-    _dump(sample_ws_flex, os.path.join(save_root, "ws_flex-paper.csv"), overwrite)
+    _dump(sample_harary, os.path.join(save_root, "harary-paper.csv.gz"), overwrite)
+    _dump(sample_ring, os.path.join(save_root, "ring-paper.csv.gz"), overwrite)
+    _dump(sample_er, os.path.join(save_root, "er-paper.csv.gz"), overwrite)
+    _dump(sample_ba, os.path.join(save_root, "ba-paper.csv.gz"), overwrite)
+    _dump(sample_ws, os.path.join(save_root, "ws-paper.csv.gz"), overwrite)
+    _dump(sample_ws_flex, os.path.join(save_root, "ws_flex-paper.csv.gz"), overwrite)
 
 
 if __name__ == "__main__":
